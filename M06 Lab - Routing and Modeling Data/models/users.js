0f662d443373ac592b4fbe2e1cbc5390552c485e
lmurray51 <111943410+lmurray51@users.noreply.github.com>
2022-10-10 14:22:30 -0400

    Add files via upload

---
+++ M06 Lab - Routing and Modeling Data/models/users.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [8, 'Minimum password length is 8 characters'],
+  },
+  isTeacher: {
+    type: Boolean,
+    required: false
+  },
+  course_ID: {
+    type: Array,
+    required: false
+  },
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// static method to login user
+userSchema.statics.login = async function(userName, password) {
+  const user = await this.findOne({ userName });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect username');
+};
+
+
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
